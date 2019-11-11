1693fcedb2e9ce24e185ef91ca030ad3a2e56e1a
Jonatan Mata <jomata@itconvergence.com>
2019-11-11 00:54:07 -0600

    On branch master [ 1 modified file:shell_setup.sh; ].

--- scripts/shell_setup.sh
+++ scripts/shell_setup.sh
@@ -11,10 +11,10 @@ main()
 
   git clone https://github.com/robbyrussell/oh-my-zsh.git "${HOME}/.oh-my-zsh"
   git clone https://github.com/jonmatum/dotfiles.git "${HOME}/.dotfiles"
-  
+
   cd "${HOME}/.oh-my-zsh/themes" || exit ${?}
   ln -s "${HOME}/.dotfiles/zsh/cobalt2.zsh-theme" .
-  
+
   cd "${HOME}" || exit ${?}
   ln -s .dotfiles/zsh/zshrc .zshrc
   ln -s .dotfiles/tmux/tmux.conf .tmux.conf
