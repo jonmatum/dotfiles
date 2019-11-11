e6685daae4ef95de84099826e5fc2b0e4de82260
Jonatan Mata <jomata@itconvergence.com>
2019-11-10 19:47:47 -0600

    On branch master [ 1 modified file:vim_setup.sh; ].

--- scripts/vim_setup.sh
+++ scripts/vim_setup.sh
@@ -8,11 +8,15 @@ main()
 {
   cd "${HOME}" || exit ${?}
 
+  curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
   if [ -f .vimrc ]; then
     rm -rf .vimrc
   fi
 
   if [ -f '.dotfiles/vim/vimrc' ]; then
+    git pull
     ln -s '.dotfiles/vim/vimrc' '.vimrc'
   else
     vimrc='https://raw.githubusercontent.com/jonmatum/dotfiles/master/vim/vimrc'
