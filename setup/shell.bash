901ccf81aef61ffc1d359bdeaf0aaa8abe0cb046
Jonatan Mata <jonmatum@gmail.com>
2019-11-14 19:16:47 -0600

    On branch master [ 1 modified file:shell.bash; 3 deleted files:../zsh/_static/aliases,../zsh/_static/git,../zsh/_static/miscellaneous; ].

--- setup/shell.bash
+++ setup/shell.bash
@@ -5,12 +5,12 @@ EPOCH=$(date +%s)
 
 function echo_msg()
 {
-  echo "- ${@:-}"
+  echo "- ${1:-}"
 }
 
 function echo_err()
 {
-  echo "ERROR: ${@:-Unable to configure dotfiles}"
+  echo "ERROR: ${1:-Unable to configure dotfiles}"
 }
 
 function install_homebrew()
@@ -21,9 +21,9 @@ function install_homebrew()
 # TODO::Add OS support to install software using, brew or any linux package manager like apt or yum.
 function main()
 {
-  cd ${HOME} || exit ${?}
+  cd "${HOME}" || exit ${?}
 
-  local b p s c f
+  local b p s c f h
 
   p='.dotfiles'
 
@@ -60,7 +60,6 @@ function main()
 
   echo_msg "Link .dotfiles"
   cd "${HOME}" || exit ${?}
-  
   for i in zshrc tmux vimrc;do
     if [[ -f "${p}/${i}/${i}.conf" ]]; then
       ln -s "${p}/${i}/${i}.conf" ".${i}.conf"
@@ -73,19 +72,26 @@ function main()
       fi
     fi
   done
+ 
+  echo_msg "Init local helpers"
+  h="${HOME}/.dotfiles_helpers"
+  if [[ ! -d "${h}" ]]; then
+    mkdir -p "${h}"
+    for i in scripts aliases git miscellaneous;do
+      cp -r "${p}/helpers/${i}" "${h}"
+    done
+  fi
 
+  echo_msg "link shell integrations"
   f=iterm2_shell_integration
-  echo_msg "link ${f}"
-
   for i in bash zsh;do
     x="${p}/iterm/${f}.${i}"
     if [[ -f "${x}" ]];then
-      ln -fs "${x}" ".${f}.${i}" 
+      ln -fs "${x}" ".${f}.${i}"
     fi
   done
 
   s="$(command -v zsh)"
-  
   if [[ -f "${s}" ]];then
     echo_msg "Change user shell to zsh"
     chsh -s "${s}"
