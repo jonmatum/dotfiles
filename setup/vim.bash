6206ecb778209ac7fafe8a7986169614a202a6ed
Jonatan Mata <jonmatum@gmail.com>
2023-05-26 15:08:56 -0600

    Update vim.bash

--- setup/vim.bash
+++ setup/vim.bash
@@ -1,28 +1,53 @@
 #!/usr/bin/env bash
-# Copyright (c) 2019 Jonatan Mata
+# dotfiles installation script
+# Author: Jonatan Mata
+# Date: 2023-05-26
 
-main()
-{
-  cd "${HOME}" || exit ${?}
+set -euo pipefail
 
+# Function to print informational messages
+function echo_msg() {
+  echo "- ${1:-}"
+}
+
+# Function to print error messages
+function echo_err() {
+  echo "ERROR: ${1:-Unable to configure dotfiles}"
+}
+
+# Main function
+function main() {
+  cd "${HOME}" || exit $?
+
+  # Install vim-plug
+  echo_msg "Installing vim-plug"
   curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-  if [ -f .vimrc ]; then
-    rm -rf .vimrc
+  # Remove existing .vimrc if present
+  if [[ -f ".vimrc" ]]; then
+    echo_msg "Removing existing .vimrc"
+    rm -f ".vimrc"
   fi
 
-  if [ -f '.dotfiles/vim/vimrc' ]; then
-    git pull
-    ln -s '.dotfiles/vim/vimrc' '.vimrc'
+  # Link or download .vimrc
+  if [[ -f ".dotfiles/vim/vimrc" ]]; then
+    echo_msg "Updating dotfiles repository"
+    git -C ".dotfiles" pull
+    echo_msg "Linking .vimrc"
+    ln -s ".dotfiles/vim/vimrc" ".vimrc"
   else
-    vimrc='https://raw.githubusercontent.com/jonmatum/dotfiles/master/vim/vimrc'
-    curl -fsSL "${vimrc}" > '.vimrc'
+    vimrc_url="https://raw.githubusercontent.com/jonmatum/dotfiles/master/vim/vimrc"
+    echo_msg "Downloading .vimrc"
+    curl -fsSL "${vimrc_url}" >".vimrc"
   fi
 
+  # Install plugins using vim-plug
+  echo_msg "Installing vim plugins"
   vi -c 'PlugInstall' +qall
 }
 
+# Execute the main function
 main
 
-exit ${?}
+exit $?
