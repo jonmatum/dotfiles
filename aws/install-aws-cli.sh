3c0af15047228bf80b467bd3e749eee90d47ec10
Jonatan Mata <Jonatan.Mata@mdlz.com>
2023-05-26 18:57:14 -0600

    test(bash): AWS CLI Install
    Signed-off-by: Jonatan Mata <Jonatan.Mata@mdlz.com>

--- aws/install-aws-cli.sh
+++ aws/install-aws-cli.sh
@@ -15,7 +15,6 @@ get_latest_aws_cli_version() {
     echo "$awscli_latest_version"
 }
 
-
 # Function to install AWS CLI
 install_aws_cli() {
     echo "Installing AWS CLI..."
@@ -33,49 +32,16 @@ if is_aws_cli_installed; then
     echo "AWS CLI is already installed (current version: $current_version)."
 
     if [ "$current_version" != "$latest_version" ]; then
-        echo "Updating AWS CLI to the latest version ($latest_version)..."
-        sudo aws configure set cli_auto_prompt on
-        sudo aws configure set cli_pager ""
-        sudo aws configure set cli_follow_urlparam false
-        sudo aws configure set cli_timestamp_format default
-        sudo aws configure set default.region us-east-1
-        sudo aws configure set default.output json
-        sudo aws configure set default.color off
-        sudo aws configure set default.s3.max_concurrent_requests 50
-        sudo aws configure set default.s3.multipart_threshold 64MB
-        sudo aws configure set default.s3.multipart_chunksize 16MB
-        sudo aws configure set default.s3.max_bandwidth 50MB/s
-        sudo aws configure set default.s3.max_queue_size 10000
-        sudo aws configure set default.s3.multipart_threshold 64MB
-        sudo aws configure set default.s3.multipart_chunksize 16MB
-        sudo aws configure set default.s3.max_bandwidth 50MB/s
-        sudo aws configure set default.s3.max_concurrent_requests 50
-        sudo aws configure set default.s3.max_queue_size 10000
-        sudo aws configure set default.s3.max_retries 10
-        sudo aws configure set default.s3.max_backoff 300
-        sudo aws configure set default.s3.s3utils ""
-        sudo aws configure set default.s3api.max_concurrent_requests 50
-        sudo aws configure set default.s3api.max_queue_size 10000
-        sudo aws configure set default.s3api.max_bandwidth 50MB/s
-        sudo aws configure set default.s3api.max_retries 10
-        sudo aws configure set default.s3api.max_backoff 300
-        sudo aws configure set default.s3api.s3utils ""
-        sudo aws configure set default.s3api.multipart_threshold 64MB
-        sudo aws configure set default.s3api.multipart_chunksize 16MB
-        sudo aws configure set default.s3api.multipart_max_parts 10000
-        sudo aws configure set default.s3api.multipart_max_part_num 10000
-        sudo aws configure set default.s3api.multipart_max_pool_connections 10
-        sudo aws configure set default.s3api.multipart_threshold 64MB
-        sudo aws configure set default.s3api.multipart_chunksize 16MB
-        sudo aws configure set default.s3api.multipart_max_parts 10000
-        sudo aws configure set default.s3api.multipart_max_part_num 10000
-        sudo aws configure set default.s3api.multipart_max_pool_connections 10
-        sudo aws configure set default.s3api.multipart_threshold 64MB
-        sudo aws configure set default.s3api.multipart_chunksize 16MB
-        sudo aws configure set default.s3api.multipart_max_parts 10000
-        sudo aws configure set default.s3api.multipart_max_part_num 10000
-        sudo aws configure set default.s3api.multipart_max_pool_connections 10
-        echo "AWS CLI updated successfully to version $latest_version."
+        echo "A newer version of AWS CLI is available ($latest_version)."
+        read -p "Do you want to update AWS CLI? (y/n): " update_choice
+
+        if [ "$update_choice" == "y" ]; then
+            echo "Updating AWS CLI to the latest version ($latest_version)..."
+            install_aws_cli
+            echo "AWS CLI updated successfully to version $latest_version."
+        else
+            echo "Skipping AWS CLI update."
+        fi
     else
         echo "AWS CLI is already up to date."
     fi
