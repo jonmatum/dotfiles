b1ce5aa9072470918885b091c866aaa0903c2d28
Jonatan Mata <jonmatum@gmail.com>
2023-05-27 00:00:03 -0600

    Doc/refactor (#14)
    * Testing installation
    
    ---------
    
    Signed-off-by: Jonatan Mata <Jonatan.Mata@mdlz.com>

--- install.sh
+++ install.sh
@@ -7,11 +7,14 @@
 
 set -euo pipefail
 
+# Define log file path
+log_file="/tmp/dotfiles-installation-$(date +%s).log"
+
 # Function to print informational messages in cyan color and redirect to log
 function echo_msg() {
     local cyan='\033[0;36m'
     local reset='\033[0m'
-    echo -e "${cyan}- ${1:-}${reset}"
+    echo -e "${cyan}- ${1:-}${reset}" | tee -a "$log_file"
 }
 
 # Function to confirm user's choice
@@ -38,25 +41,28 @@ function execute_script() {
     local script_url=$1
     local script_name=${script_url##*/}
     local script_path="/tmp/$script_name"
-
-    # Download the script
-    echo_msg "Downloading $script_name..."
-    curl -sSL -o "$script_path" "$script_url"
-
-    # Make the script executable
-    chmod +x "$script_path"
-
+    
     # Prompt user for confirmation
     if confirm "Do you want to run $script_name?"; then
-        # Execute the script
-        echo_msg "Running $script_name..."
-        "$script_path" >/tmp/${script_name}-$(date +%s).log 2>&1
+        (
+            # Download the script
+            echo_msg "Downloading $script_name..."
+
+            curl -sSL -o "$script_path" "$script_url"
+
+            # Make the script executable
+            chmod +x "$script_path"
+
+            # Execute the script
+            echo_msg "Running $script_name..."
+            "$script_path"
+        ) >>"$log_file" 2>&1
+
+        # Remove the script
+        rm "$script_path"
     else
         echo_msg "Skipping $script_name..."
     fi
-
-    # Remove the script
-    rm "$script_path"
 }
 
 # Execute additional scripts
