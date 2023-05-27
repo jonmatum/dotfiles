68bae7c54d662945f2f9b735b8047d8e75b77030
Jonatan Mata <Jonatan.Mata@mdlz.com>
2023-05-27 10:56:50 -0600

    Testing modular setup
    Signed-off-by: Jonatan Mata <Jonatan.Mata@mdlz.com>

--- zsh/setup-zsh.bash
+++ zsh/setup-zsh.bash
@@ -252,6 +252,6 @@ function install_zsh_theme() {
 configure_file "https://raw.githubusercontent.com/jonmatum/dotfiles/main/zsh/.zshrc" "${HOME}" "${HOME}/.dotfiles_backups"
 install_fonts
 configure_zsh
+install_zsh_theme "https://raw.githubusercontent.com/jonmatum/dotfiles/main/zsh/me.zsh-theme"
 configure_python
 configure_shell
-install_zsh_theme "https://raw.githubusercontent.com/jonmatum/dotfiles/main/zsh/me.zsh-theme"
