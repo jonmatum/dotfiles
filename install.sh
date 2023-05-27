f75059202e9d712e0b9855ba33c71558cf474ac4
Jonatan Mata <Jonatan.Mata@mdlz.com>
2023-05-26 22:54:56 -0600

    Testing installation
    Signed-off-by: Jonatan Mata <Jonatan.Mata@mdlz.com>

--- install.sh
+++ install.sh
@@ -29,7 +29,7 @@ function execute_script() {
 
     # Execute the script
     echo_msg "Running $script_name..."
-    "$script_path"
+    "$script_path" >/dev/null
 
     # Remove the script
     rm "$script_path"
