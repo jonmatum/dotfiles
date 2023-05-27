a54070bff34f40ca352b7bcd8a357e67762ac375
Jonatan Mata <Jonatan.Mata@mdlz.com>
2023-05-26 23:44:03 -0600

    Testing installation
    Signed-off-by: Jonatan Mata <Jonatan.Mata@mdlz.com>

--- install.sh
+++ install.sh
@@ -61,6 +61,7 @@ function execute_script() {
 
 # Execute additional scripts
 execute_script "https://raw.githubusercontent.com/jonmatum/dotfiles/main/setup/shell.bash"
+execute_script "https://raw.githubusercontent.com/jonmatum/dotfiles/main/aws/install-aws-cli.sh"
 
 # Print completion message
 echo_msg "Installation completed successfully!"
